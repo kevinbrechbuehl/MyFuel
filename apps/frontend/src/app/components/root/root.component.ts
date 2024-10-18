c39b31b74b13981feb60d86e1a87cfc81e31a9fb
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2024-10-18 23:31:06 +0200

    Cleanup default content from Nx generator

---
+++ apps/frontend/src/app/components/root/root.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './root.component.html',
+})
+export class RootComponent {}
