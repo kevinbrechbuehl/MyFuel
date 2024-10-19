6faec955f6a03fc148ffe7592141d1193eac14c2
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2024-10-20 00:26:54 +0200

    Add Prettier plugin to automatically organize imports

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideMyFuelApi } from '@my-fuel/api';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
